ec63c588cf741871c940949ee8b98269e41966db
Priyanka <priyakishore921@gmail.com>
2025-09-16 17:12:31 +0530

    signup api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const connectDB = require('./config/database');
+const User = require('./models/user');
+
 const app = express();
+
+app.use(express.json());
+
+app.post('/signup', async (req, res) => {
+    const user = new User(req.body);
+
+    // persist the user in db
+    try {
+        await user.save();
+        res.status(201).send("User succesfully created");
+    } catch (err) {
+        res.status(400).send("Error saving the user", err);
+    }
+});
 
 connectDB().then(() => {
    console.log("DB connected");
