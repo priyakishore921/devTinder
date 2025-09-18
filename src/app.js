82e58c81c62cf561a33cf44065f1d6862894a607
Priyanka <priyakishore921@gmail.com>
2025-09-18 17:09:22 +0530

    add profile and login apis, use password hashing for signup, use jwt token for login api, parse cookie for get profile api

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const connectDB = require('./config/database');
 const User = require('./models/user');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.post('/signup', async (req, res) => {
     const user = new User(req.body);
@@ -12,6 +16,9 @@ app.post('/signup', async (req, res) => {
 
     // persist the user in db
     try {
+        const password = req.body.password;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        user.password = hashedPassword;
         await user.save();
         res.status(201).send("User succesfully created");
     } catch (err) {
@@ -19,6 +26,52 @@ app.post('/signup', async (req, res) => {
     }
 });
 
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if(!user) {
+            return res.status(404).send("Invalid email");
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if(!isPasswordMatch) {
+            return res.status(400).send("Invalid credentials");
+        }
+
+        // create a jwt token and send it to user as cookie
+        const token = jwt.sign({_id: user._id}, "FirstJWTSecret");
+        console.log("token", token);
+        res.cookie("token", token);
+        res.send("User logged in successfully");
+    }
+    catch(err) {
+        res.status(500).send("Error logging in the user"+ err.message);
+    }
+})
+
+app.get('/profile', async (req, res) => {
+    try {
+        console.log('cookie', req.cookies);
+        // validate the cookie
+        const {token} = req.cookies;
+        if(!token) {
+            return res.status(401).send("Unauthorized");
+        }
+        // validate token
+        const { _id } = jwt.verify(token, "FirstJWTSecret");
+        // validate user
+        const user = await User.findById(_id);
+        if(!user) {
+            return res.status(404).send("User not found");
+        }
+        console.log("user", user);
+        res.send(user);
+    }
+    catch(err) {
+        res.status(500).send("Error fetching profile"+ err.message);
+    }
+});
 
 // find user by email
 app.get('/user', async (req, res) => {
