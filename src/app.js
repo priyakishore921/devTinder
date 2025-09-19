04968035f18b1c0615b454efcd021287447198bf
Priyanka <priyakishore921@gmail.com>
2025-09-19 12:10:55 +0530

    created routes folder and moved auth routes to auth router

--- src/app.js
+++ src/app.js
@@ -6,49 +6,14 @@ const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
 const { UserAuth } = require('./middlewares/auth');
 
+const authRouter = require('./routes/auth');
+
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.post('/signup', async (req, res) => {
-    const user = new User(req.body);
-    console.log(req.body);
-
-    // persist the user in db
-    try {
-        const password = req.body.password;
-        const hashedPassword = await bcrypt.hash(password, 10);
-        user.password = hashedPassword;
-        await user.save();
-        res.status(201).send("User succesfully created");
-    } catch (err) {
-        res.status(400).send("Error saving the user"+ err.message);
-    }
-});
-
-app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.findOne({ email });
-        if(!user) {
-            return res.status(404).send("Invalid email");
-        }
-
-        const isPasswordMatch = user.validatePassword(password);
-        if(!isPasswordMatch) {
-            return res.status(400).send("Invalid credentials");
-        }
-
-        const token = await user.getJWT();
-
-        res.cookie("token", token, {expires: new Date(Date.now() + 7*3600000), httpOnly: true});
-        res.send("User logged in successfully");
-    }
-    catch(err) {
-        res.status(500).send("Error logging in the user"+ err.message);
-    }
-})
+app.use('/', authRouter);
 
 app.get('/profile', UserAuth, async (req, res) => {
     try {
---
+++ src/routes/auth.js
@@ -0,0 +1,60 @@
+/* This module define routes of the below end points -
+* - POST /signup
+* - POST /login
+* - POST /logout
+*/
+
+const express = require('express');
+const authRouter = express.Router();
+const User = require('../models/user');
+const bycrypt = require('bcrypt');
+
+authRouter.post('/signup', async (req, res) => {
+    // receives various details from signup form
+    // hashes the password using bcrypt
+    // saves the user in db
+    // sends a success response
+
+    try {
+        const user = new User(req.body);
+        const password = req.body.password;
+        const hashedPassword = await bycrypt.hash(password, 10);
+        user.password = hashedPassword;
+
+        user.save();
+        res.status(201).send("User succesfully created");
+    }
+    catch(err) {
+        res.status(500).send("Error signing up the user"+ err.message);
+    }
+});
+
+authRouter.post('/login', async (req, res) => {
+    // receive email and password from login form
+    // validate email
+    // if email exists, validate password using bycrypt.verify
+    // if password i valid, generate a JWT token using jsonwebtoken
+    // send the token in cookie
+    // send success response
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        if(!user) {
+            throw new Error("Invaid credentials");
+        }
+        
+        const isPasswordMatch = await bycrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            throw new Error("Invalid credentials");
+        }
+
+        const token = await user.getJWT();
+        res.cookie("token", token, {expires: new Date(Date.now() + 7*3600000), httpOnly: true});
+        res.send("USer logged in sucecssfully!");
+    }
+    catch(err) {
+        res.status(500).send("Error signing in the user "+ err.message);
+    }
+});
+
+module.exports = authRouter;
